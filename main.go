ef3d7ce8b3da8658e013350bcf491821dad63424
kris686-stack <60702260+kris686-stack@users.noreply.github.com>
2020-05-14 12:41:35 +0530

    httpClientAndServer Code
    Learners Guide

--- main.go
+++ main.go
@@ -1,24 +1,55 @@
-// mockHttpServer project main.go
+// excercise14.1 project main.go
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-type display struct{}
+type msgData struct {
+	Message string `json:"message"`
+}
 
-func (h display) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	data := []byte("hello\n")
-	res.Write(data)
-	sasi := []byte("unni malu\n")
-	res.Write(sasi)
-	io.WriteString(res, "IN CANADA\n")
-	fmt.Fprint(res, "Welcome to canada ,nice to meet you \n")
+func getDataAndReturnResponse() string {
+	r, err := http.Get("https://img1.exportersindia.com/product_images/bc-small/2019/9/1448281/wild-fish-1569326265-5093788.jpeg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	return string(data)
+}
+func getStructDataAndRes() msgData {
+	r, err := http.Get("http://localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	defer r.Body.Close()
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	msg := msgData{}
+	err = json.Unmarshal(data, &msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return msg
 }
 
 func main() {
-	handler := display{}
-	http.ListenAndServe(":8000", handler)
+	fmt.Println("Hello World!")
+	//data := getDataAndReturnResponse()
+	//fmt.Println(data)
+	d := getStructDataAndRes()
+	fmt.Println(d.Message)
 }
